769032de9a68baf433fe4482e327a4cc89f01ec6
Kumar Gaurav <5073326+mrdotkg@users.noreply.github.com>
2023-09-10 14:07:52 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-document.head.innerHTML = document.head.innerHTML + '<meta name="theme-color" content="rgba(33, 33, 33, 0.98)">'
 var link = document.createElement('meta');
 link.setAttribute('name', 'theme-color');
 link.content = "rgba(33, 33, 33, 0.98)";
